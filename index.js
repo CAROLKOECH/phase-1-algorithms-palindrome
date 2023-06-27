f577177904e775ea57a4c15ab7a7682d1cd512df
CAROLKOECH <koechc44@gmail.com>
2023-06-28 02:05:00 +0300

    Done with assignment

--- index.js
+++ index.js
@@ -1,25 +1,40 @@
 function isPalindrome(word) {
-  // Write your algorithm here
+  const lowercaseWord = word.toLowerCase();
+  let start = 0;
+  let end = lowercaseWord.length - 1;
+  
+  while (start < end) {
+    if (lowercaseWord[start] !== lowercaseWord[end]) {
+      return false;
+    }
+    
+    start++;
+    end--;
+  }
+  
+  return true;
 }
 
-/* 
-  Add your pseudocode here
-*/
+// Test Cases
+console.log("Expecting: true");
+console.log("=>", isPalindrome("abba"));
 
-/*
-  Add written explanation of your solution here
-*/
+console.log("");
 
-// You can run `node index.js` to view these console logs
-if (require.main === module) {
-  // add your own custom tests in here
-  console.log("Expecting: true");
-  console.log("=>", isPalindrome("racecar"));
+console.log("Expecting: true");
+console.log("=>", isPalindrome("racecar"));
 
-  console.log("");
+console.log("");
 
-  console.log("Expecting: false");
-  console.log("=>", isPalindrome("robot"));
-}
+console.log("Expecting: true");
+console.log("=>", isPalindrome("a"));
 
-module.exports = isPalindrome;
+console.log("");
+
+console.log("Expecting: false");
+console.log("=>", isPalindrome("robot"));
+
+console.log("");
+
+console.log("Expecting: false");
+console.log("=>", isPalindrome("ab"));
